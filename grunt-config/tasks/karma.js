ce8afc368af59cb5daa7d258446b761561b83025
Kyle Xiong <Jiaji.Xiong@genesys.com>
2015-03-31 16:13:57 +0800

    Remove unnecessary codes for karma test task.

--- grunt-config/tasks/karma.js
+++ grunt-config/tasks/karma.js
@@ -3,63 +3,6 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-karma');
 
-  var argv = require('optimist').argv;
-
-  var buildDir = require('../buildDir.js')();
-
-
-  var generateFiles = function() {
-    var filePatterns = [
-        // 'src/lib/jquery-2.1.3.js',
-        //'src/lib/angular.js',
-        //'src/lib/angular-local-storage.js',
-        //'src/lib/jasmine-jquery.js',
-        //{pattern: 'dashboard/tests/spec/**/*.js', included: false},
-        {pattern: 'Angular-Grunt-Boilerplate/target/build/webapp/**/*.*', included: false},
-        {pattern: 'Angular-Grunt-Boilerplate/src/modules/**/*.jade', included: false},
-
-        {
-            pattern: 'Angular-Grunt-Boilerplate/src/modules/core/locale/*.json',
-            served: true,
-            included: false
-        },
-      ];
-
-      var includePatterns = argv.i;
-      if (includePatterns) {
-          if (!Array.isArray(includePatterns)) {
-              includePatterns = [includePatterns];
-          }
-          includePatterns.forEach(function(includePattern) {
-              filePatterns.push(
-                { pattern: 'Angular-Grunt-Boilerplate/tests/spec/**/*' + includePattern + '*.js', included: false }
-              )
-          });
-      } else {
-          filePatterns.push(
-            { pattern: 'Angular-Grunt-Boilerplate/tests/spec/**/*.js', included: false }
-          );
-      }
-
-      return filePatterns;
-  };
-
-  var getExcludeFiles = function() {
-      var res = [];
-      var excludePatterns = argv.x;
-      if (excludePatterns) {
-          if (!Array.isArray(excludePatterns)) {
-              excludePatterns = [excludePatterns];
-          }
-          excludePatterns.forEach(function(excludePattern) {
-              res.push(
-                'Angular-Grunt-Boilerplate/tests/spec/**/*' + excludePattern + '*.js'
-              )
-          });
-      }
-      return res;
-  };
-
   grunt.config('karma',{
     options: {
             basePath: '../',
@@ -78,7 +21,7 @@ module.exports = function(grunt) {
 
             browsers: ['PhantomJS'],
 
-            exclude: getExcludeFiles(),
+            exclude: [],
 
             files: [
               'Angular-Grunt-Boilerplate/target/build/webapp/app.js',
